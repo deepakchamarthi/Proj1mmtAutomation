64ad6224fb1934f5c323c6da643a7865f67ee374
Deepak Chamarthi <chdeepak@hotmail.com>
2021-08-29 18:06:50 +0530

    Add files via upload
    changed constat folder

---
+++ src/main/java/mmt/constants/StarRating.java
@@ -0,0 +1,14 @@
+package mmt.constants;
+
+
+/**
+ * 
+ * @author Deepak
+ *
+ */
+
+//Enum to get these rating values only. 
+public enum StarRating{
+
+	 RATING1,RATING2,RATING3,RATING4,RATING5
+}
